5a722e1511e8d1178e29962ea8190d71e6dab637
Vbasov <0120912@mail.ru>
2022-10-02 11:47:15 +0300

    add 009 dz

--- 009/dz.py
+++ 009/dz.py
@@ -1,10 +1,11 @@
-n = 3
+# 18). Реализуйте алгоритм перемешивания списка.
+import random
 
-def get_numbers(n):
-    list = []
-    for i in range(-n, n + 1):
-        list.append(i)
-    return list
+list = [2, 7, 8, 6, 1]
+print ("Оригинальный список: " + str(list))
 
-list = get_numbers(n)
-print (list)
+for i in range(len(list)-1, 0, -1):
+    j = random.randint(0, i + 1)
+    list[i], list[j] = list[j], list[i]
+     
+print ("Смешанный список: " +  str(list))
