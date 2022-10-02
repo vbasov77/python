f163f760ece249dfd587238aecca9cee601be618
Vbasov <0120912@mail.ru>
2022-10-02 09:09:38 +0300

    add 005 dz

--- 001/lec.py
+++ 001/lec.py
@@ -4,7 +4,9 @@
 a = int(input('введите число 1 - 7 '))
 if a < 1 or a > 7:
     print ('Нет такого дня')
-elif a >= 1 and a <= 5: 
+    a = int(input('введите число 1 - 7 '))
+
+if a >= 1 and a <= 5: 
     print('Нет') 
 else:
     print('Да')
