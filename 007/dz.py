203d8d3bab74a8ec68be4357e2517eb92a9a80e8
Vbasov <0120912@mail.ru>
2022-10-05 08:22:45 +0300

    update old file 007

--- 007/dz.py
+++ 007/dz.py
@@ -1,7 +1,25 @@
 # 16. Задать список из n чисел последовательности (1+1/n)^n 
 # и вывести на экран их сумму 
 
-n = int(input('Введите число'))
-result = [round((1 + 1 / i) ** i, 3) for i in range(1, n + 1)]
-print (result)
-print (round(sum(result), 3))
+# n = int(input('Введите число'))
+# result = [round((1 + 1 / i) ** i, 3) for i in range(1, n + 1)]
+# print (result)
+# print (round(sum(result), 3))
+
+# Второе решение
+
+while True:
+    num = input('Введите целое положительное число:  ')
+    try:
+        num = int(num)
+        if num > 0:
+            break
+        else: print('Число отрицательное')
+    except ValueError:
+        print('Число должно быть положительным')
+list = []
+for i in range(1, num + 1):
+    elem = round(((1 + 1/i)**i), 2)
+    list.append(elem)
+print(list)
+print(f'Сумма последовательности равна: {sum(list)}')
